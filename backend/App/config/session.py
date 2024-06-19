5970c9a3037f03656ce1ea1896bd2508bf43c2ed
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-06-19 17:38:03 -0400

    Create  Virtual Envoroment for the project

--- backend/App/config/session.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-
-class DatabaseSessionContext(object):
-    def __init__(self, db: Session):
-        self.db = db
-
-class AppService(DatabaseSessionContext):
-    pass
